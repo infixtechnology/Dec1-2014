969350eccdc3db28dbad5a45ae1237008e65bcbc
Reaz Patwary <infixtechnology@gmail.com>
2014-12-01 16:17:38 -0500

    Adding a new project

---
+++ VeriablePrac/src/com/infix/qa/AdditionPratc.java
@@ -0,0 +1,14 @@
+package com.infix.qa;
+
+public class AdditionPratc {
+
+	public static void main(String[] args) {
+		int firstNum; // variable declaration
+		firstNum = 9;// Assiging a value to variable
+		int secNum = 8;
+		float result = firstNum / secNum;
+		System.out.println("The final result is "+result);
+
+	}
+
+}
